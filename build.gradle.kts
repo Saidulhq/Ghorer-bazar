f4decdd581c21b046e22713f89d9b7a7a45c75f7
Saidulhq <saidul.clc@gmail.com>
2026-09-07 05:28:37 +0600

    Update build.gradle.kts to remove plugins
    Removed Android and Kotlin plugin declarations.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-plugins {
-    id("com.android.application") version "8.5.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+dependencies {
+    implementation("androidx.compose.material:material-icons-extended:1.6.0")
 }
